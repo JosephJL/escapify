291edc5b25fb0ee011d5e0ffa71649f99aa1d15a
JosephJL <HeeJLJoseph@gmail.com>
2022-10-07 11:04:19 +0800

    removed Food stuff

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import MainPage from '../views/MainPage.vue'
-import { getAuth } from 'firebase/auth'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import MainPage from "../views/MainPage.vue";
+import { getAuth } from "firebase/auth";
 
-const requireAuth = (to,from,next) => {
-  let user = getAuth().currentUser
-  console.log("current user in auth guard: ", user)
-  if(!user && to.name != 'Home') next({name:'Home'})
-  else next()
-}
+// const requireAuth = (to,from,next) => {
+//   let user = getAuth().currentUser
+//   console.log("current user in auth guard: ", user)
+//   if (!user && to.name != "MainPage") next({ name: "MainPage" });
+//   else next();
+// }
 
 // router.beforeEach(async (to,from,next) => {
 //   //make sure user is not null
@@ -18,22 +18,21 @@ const requireAuth = (to,from,next) => {
 
 const routes = [
   {
-    path:'/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home,
   },
   {
-    path:'/mainPage',
-    name:'MainPage',
-    component:MainPage,
+    path: "/mainPage",
+    name: "MainPage",
+    component: MainPage,
     beforeEnter: requireAuth,
-  }
-]
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-
-export default router
+export default router;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,39 +1,42 @@
 <template>
-    <div class="homecontainer">
-        <p>Home screen here</p>
-        <div v-if="showLogin">
-            <h2>Login</h2>
-            <LoginForm @login="enterMain" />
-            <p>No account yet? <span @click="showLogin = false">Signup </span>instead</p>
-        </div>
-        <div v-else>
-            <h2>Sign up</h2>
-            <SignUpForm @signup="enterMain" />
-            <p>Already registered? <span @click="showLogin = true">Login </span>instead</p>
-        </div>
+  <div class="homecontainer">
+    <p>Home screen here</p>
+    <div v-if="showLogin">
+      <h2>Login</h2>
+      <LoginForm @login="enterMain" />
+      <p>
+        No account yet? <span @click="showLogin = false">Signup </span>instead
+      </p>
     </div>
+    <div v-else>
+      <h2>Sign up</h2>
+      <SignUpForm @signup="enterMain" />
+      <p>
+        Already registered? <span @click="showLogin = true">Login </span>instead
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
-import {ref} from 'vue'
-import SignUpForm from '../components/SignUpForm.vue'
-import LoginForm from '../components/LoginForm.vue'
-import {useRouter} from 'vue-router'
-import MainPage from '../views/MainPage.vue'
-
+import { ref } from "vue";
+import SignUpForm from "../components/auth/SignUpForm.vue";
+import LoginForm from "../components/auth/LoginForm.vue";
+import { useRouter } from "vue-router";
+import MainPage from "../views/MainPage.vue";
 
 export default {
-    components:{SignUpForm, LoginForm},
+  components: { SignUpForm, LoginForm },
 
-    setup() {
-        const showLogin = ref(true)
-        const router = useRouter()
+  setup() {
+    const showLogin = ref(true);
+    const router = useRouter();
 
-        const enterMain = () => {
-            router.push({path: '/mainPage'})
-        }
+    const enterMain = () => {
+      router.push({ path: "/mainPage" });
+    };
 
-        return {showLogin, enterMain}
-    }
-}
+    return { showLogin, enterMain };
+  },
+};
 </script>
