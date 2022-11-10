d5e1f66abfe5610ae1d29063f09b3bf54651a3e6
JosephJL <HeeJLJoseph@gmail.com>
2022-11-10 17:37:21 +0800

    Update Styling
    Add Component to home page

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,13 @@
 <template>
   <nav
-    class="navbar navbar-expand sticky-top"
-    style="background-color: #dfe9f5"
+    class="navbar navbar-expand bg-dark sticky-top"
+    style="background-color: #dfe9f5; --bs-bg-opacity: 0.98"
   >
     <!-- old color #e3f2fd  -->
     <div class="container-fluid">
-      <a class="navbar-brand" href="#" @click.prevent="goHome">Escape</a>
+      <a class="navbar-brand text-white" href="#" @click.prevent="goHome"
+        >Escape</a
+      >
       <!-- <button
         class="navbar-toggler"
         type="button"
@@ -20,23 +22,43 @@
 
       <ul class="navbar-nav ms-auto mb-0">
         <!-- dropdown when user is logged in -->
-        <a class="navbar-brand" href="#" @click.prevent="goHome"
-          ><span style="font-size: 16px">Explore</span></a
+        <a class="navbar-brand text-white" href="#" @click.prevent="goHome"
+          ><span style="font-size: 16px">Discover</span></a
         >
-        <a class="navbar-brand" href="#" @click.prevent="openCommunityPage"
+        <a
+          class="navbar-brand text-white"
+          href="#"
+          @click.prevent="openCommunityPage"
           ><span style="font-size: 16px">Community</span></a
         >
         <template v-if="user">
-          <li class="nav-item">
+          <li class="nav-item text-white">
             <div class="dropdown">
               <a
-                class="btn border rounded-pill"
+                class="btn border rounded-pill d-flex"
                 href="#"
                 role="button"
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
               >
-                <span class="navbar-toggler-icon"></span>
+                <div>
+                  <span class="navbar-toggler-icon navbar-dark me-2"></span>
+                </div>
+                <div class="text-light">
+                  <svg
+                    v-if="user"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-person"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"
+                    />
+                  </svg>
+                </div>
               </a>
               <ul class="dropdown-menu dropdown-menu-end mt-2">
                 <li>
@@ -73,11 +95,9 @@
                 data-bs-toggle="dropdown"
                 aria-expanded="false"
               >
-                <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon navbar-dark"></span>
               </a>
               <ul class="dropdown-menu dropdown-menu-end mt-2">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <hr />
                 <li>
                   <button
                     type="button"
