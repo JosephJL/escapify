cc4d4bedc4479bc4d0629f9b3d1ac6e806951d0f
Blobsterdude <domtam810@hotmail.com>
2022-11-12 04:50:53 +0800

    Update Footer.vue
    Fixed footer links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,8 +6,7 @@
       <!--Grid row-->
       <MDBRow>
         <!--Grid column to fill up some empty space-->
-        <MDBCol lg="1">
-        </MDBCol>
+        <MDBCol lg="1"> </MDBCol>
         <!-- Grid column -->
         <MDBCol lg="6" md="12" class="mb-4 mb-md-0">
           <h5 class="text-uppercase">About Us</h5>
@@ -32,20 +31,29 @@
               alt=""
               style="width: 20px"
             />
-            Escapify Pte Ltd<br /><br>
+            Escapify Pte Ltd<br /><br />
             <img
               src="https://icon-library.com/images/facebook-icon-circle-png/facebook-icon-circle-png-3.jpg"
               alt=""
               style="width: 20px"
             />
-            <a href="www.facebook.com" style="color:white;text-decoration:none;"> Facebook</a>
+            <a
+              href="https://www.facebook.com"
+              style="color: white; text-decoration: none"
+            >
+              Facebook</a
+            >
             <br />
             <img
               src="https://icon-library.com/images/instagram-icon-logo/instagram-icon-logo-29.jpg"
               alt=""
               style="width: 30px"
             />
-            <a href="www.instagram.com" style="color:white;text-decoration:none;">Instagram</a>
+            <a
+              href="https://www.instagram.com"
+              style="color: white; text-decoration: none"
+              >Instagram</a
+            >
             <br />
             <br />
             <img
