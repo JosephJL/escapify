90924e8d690fb8ca7355a5e4760cd6c5eac8cf31
Blobsterdude <domtam810@hotmail.com>
2022-11-12 04:44:09 +0800

    Updated footer
    added facebook and insta links. changed col size to 6-6

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,24 +6,59 @@
       <!--Grid row-->
       <MDBRow>
         <!--Grid column-->
-        <MDBCol lg="8" md="12" class="mb-4 mb-md-0">
+        <MDBCol lg="6" md="12" class="mb-4 mb-md-0">
           <h5 class="text-uppercase">About Us</h5>
 
           <p>
-            Escapify is a destination planning website. Our goal is to bring a comprehensive travel platform where users can view all the destinations and accommodations in a single place easily and quickly. Escapify also hopes to build a close-knit community of travel enthusiasts.
+            Escapify is a destination planning website. Our goal is to bring a
+            comprehensive travel platform where users can view all the
+            destinations and accommodations in a single place easily and
+            quickly. Escapify also hopes to build a close-knit community of
+            travel enthusiasts.
           </p>
         </MDBCol>
         <!--Grid column-->
 
         <!--Grid column-->
-        <MDBCol lg="4" md="12" class="mb-4 mb-md-0">
-          <h5 class="text-uppercase">Contact Us</h5>
+        <MDBCol lg="6" md="12" class="mb-4 mb-md-0">
+          <h5 class="text-uppercase">Stay in Touch</h5>
 
           <p>
-            Escapify Pte Ltd<br>
-            80 Stamford Rd, Singapore 178902<br>
-            <br>
-            <strong>Have Questions? Email us <a href="mailto:support@escapify.com">here</a></strong>
+            <img
+              src="https://www.pngkey.com/png/full/235-2350076_gmw-host-clipart-library-people-icon-png-white.png"
+              alt=""
+              style="width: 20px"
+            />
+            Escapify Pte Ltd<br /><br>
+            <img
+              src="https://icon-library.com/images/facebook-icon-circle-png/facebook-icon-circle-png-3.jpg"
+              alt=""
+              style="width: 20px"
+            />
+            <a href="www.facebook.com" style="color:white;text-decoration:none;"> Facebook</a>
+            <br />
+            <img
+              src="https://icon-library.com/images/instagram-icon-logo/instagram-icon-logo-29.jpg"
+              alt=""
+              style="width: 30px"
+            />
+            <a href="www.instagram.com" style="color:white;text-decoration:none;">Instagram</a>
+            <br />
+            <br />
+            <img
+              src="https://icon-library.com/images/address-icon-white/address-icon-white-28.jpg"
+              alt=""
+              style="width: 25px"
+            />
+            80 Stamford Rd, Singapore 178902
+            <br />
+            <br />
+            <strong
+              >Have Questions?
+              <a href="mailto:support@escapify.com" style="color: white"
+                >Email us</a
+              ></strong
+            >
           </p>
         </MDBCol>
         <!--Grid column-->
